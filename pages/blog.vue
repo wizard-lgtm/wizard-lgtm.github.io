91e03cb2ca4ed0f5c0fd760ff3610f460540cca3
wizard-lgtm <rippiten23@gmail.com>
2024-10-31 07:51:58 +0300

    blog

---
+++ pages/blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Holy, DID YOU REALLY ENTER HERE?</h1>
+  <b>i didn't create the page</b>
+  <br />
+  <b>i'm still thinking about you really entered here, i'm so happy rn </b>
+</template>
+<style>
+body {
+  background-color: #121212;
+  color: #f6f7f7;
+}
+a {
+  color: #8c8de3;
+}
+</style>
