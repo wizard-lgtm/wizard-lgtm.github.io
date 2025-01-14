e74207b57b6cfec52dab48c9a1351463828b75fd
J <rippiten23@gmail.com>
2025-01-14 17:33:51 +0300

    Create blog-2.vue

---
+++ pages/blog-2.vue
@@ -0,0 +1,144 @@
+<template>
+<h1 id="day-2">Day 2</h1>
+<p>I&#39;m at the library again, an a diffrent one. This place is much smaller and much worse than yesterday. There&#39;s a fan fucks my mind, fortunately i have my heaphones so i don&#39;t need to hear it at all the time. </p>
+<p>I checked the computers again, which there was six, every one of them was occupied, so i had to use my rusty 2000$ worth of fucking macbook which it&#39;s useless unless watching videos/movies.</p>
+<p>I got a gum from the kid sitting my on my left. I didn&#39;t say thanks but i made a thumbsup emoji to him. Well at least i said something.</p>
+<p>Everybody is working for college here, i don&#39;t know they&#39;re enjoying of that, or i don&#39;t even know they should do that, but they&#39;re just doing. </p>
+<p>Soo, i guess i had to use that computer, i must do something with it. I wanna try that new brand Helix text editor. I&#39;m dropping the <a href="https://helix-editor.com/">link</a> over here, it&#39;s kinda vim, but it&#39;s not. Idk</p>
+<p>Soo, you might ask me &quot;WHY YOU&#39;RE USING THE PUBLIC COMPUTERS INSTEAD OF YOUR FUCKING MACBOOK PRO YOU STUPID IDIOT&quot; and i would say, &quot;yes ma&#39;am ur right but listen me first&quot;. So if i&#39;m gonna use enough of that public computers enough, i&#39;ll miss my macbook pro. At least i&#39;m trying to do that and i hope it&#39;s going to be work.</p>
+</template>
+
+<style scoped>
+@charset "utf-8";
+
+/**
+ * markdown.css
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://gnu.org/licenses/lgpl.txt.
+ *
+ * @project      Weblog and Open Source Projects of Florian Wolters
+ * @version      GIT: $Id$
+ * @package      xhtml-css
+ * @author       Florian Wolters <florian.wolters.85@googlemail.com>
+ * @copyright    2012 Florian Wolters
+ * @cssdoc       version 1.0-pre
+ * @license      http://gnu.org/licenses/lgpl.txt GNU Lesser General Public License
+ * @link         http://github.com/FlorianWolters/jekyll-bootstrap-theme
+ * @media        all
+ * @valid        true
+ */
+
+img {
+	max-width: 100%;
+}
+
+table {
+	border-collapse: collapse;
+	width: 100%;
+}
+
+table, th, td {
+	border: 1px solid #EAEAEA;
+	border-radius: 3px;
+	padding: 5px;
+}
+
+tr:nth-child(even) {
+	background-color: #F8F8F8;
+}
+
+body {
+    font-family: Helvetica, Arial, Freesans, clean, sans-serif;
+padding:1em;
+margin:auto;
+max-width:42em;
+background:#fefefe;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    font-weight: bold;
+}
+
+h1 {
+    color: #000000;
+    font-size: 28px;
+}
+
+h2 {
+    border-bottom: 1px solid #CCCCCC;
+    color: #000000;
+    font-size: 24px;
+}
+
+h3 {
+    font-size: 18px;
+}
+
+h4 {
+    font-size: 16px;
+}
+
+h5 {
+    font-size: 14px;
+}
+
+h6 {
+    color: #777777;
+    background-color: inherit;
+    font-size: 14px;
+}
+
+hr {
+    height: 0.2em;
+    border: 0;
+    color: #CCCCCC;
+    background-color: #CCCCCC;
+}
+
+p, blockquote, ul, ol, dl, li, table, pre {
+    margin: 15px 0;
+}
+
+code, pre {
+    border-radius: 3px;
+    background-color: #F8F8F8;
+    color: inherit;
+}
+
+code {
+    border: 1px solid #EAEAEA;
+    margin: 0 2px;
+    padding: 0 5px;
+}
+
+pre {
+    border: 1px solid #CCCCCC;
+    line-height: 1.25em;
+    overflow: auto;
+    padding: 6px 10px;
+}
+
+pre > code {
+    border: 0;
+    margin: 0;
+    padding: 0;
+}
+
+a, a:visited {
+    color: #4183C4;
+    background-color: inherit;
+    text-decoration: none;
+}
+
+</style>
