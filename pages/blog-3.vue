03bf038509053ef991a904b74737b84517390189
J <rippiten23@gmail.com>
2025-01-19 02:06:17 +0300

    Create blog-3.vue

---
+++ pages/blog-3.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>Day 3</h1>
+Ok, I'll tell ya.
+
+You are the worst person I know, 
+  you constantly hit on your best friend's wife,
+    the man pays for your food and rescued you from certain death and this is how you repay him?
+        And to add insult to injury you defecate all over his yard, and your such a sponge.
+You pay for nothing and you always say "Uugh, catch ya later." but later never comes!
+And what really bothers me is that you pretend your this deep guy who loves women for their souls, but all you do is date bimbos.
+Yeah, I date women for their bodies, but at least I'm honest about it.
+I don;t buy them a copy of Catcher in the Rye and then lecture them with some 7th grade interpretation about Holden Caulfield being some profound intellectual, he wasn't! He was a spoiled brat, and that's why you like him so much, he's you.
+God you're pretentious, and you delude yourself by thinking you're some great writer even though your terrible. You know, I should've known Cheryl Tiegs didn't write you that note, she would've known there is no 'a' in the word definite.
+And, I think what I hate most about you? Your Textbook Liberal Agenda, how we should "Legalize pot man..." how big business is crushing the underclass, and how homelessness is the biggest tragedy in America, well what have you done to help? I work down at the soup kitchen, Brian. Never seen you down there! You wanna to help? Grab a ladle! And by the way, driving a Prius doesn't mean your Jesus Christ, oh, wait! You don't believe in Jesus Christ or any religion for that matter, because "religion is for idiots". Well who the hell are you to talk down to anyone, you failed college twice, but isn't as nearly as bad as your failure as a father, hows your son of your's that you never see? But you know what? I could forgive all of that, all of it, if you weren't such a bore! That's the worst of it Brian, your just a big, sad, alcoholic bore. *sigh*
+
+<i>See you Brian, thanks for the f@#$ing steak.</i>
+</template>
+
