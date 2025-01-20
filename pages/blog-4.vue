2296e276002207d5186120e66b578355140d3c2a
J <rippiten23@gmail.com>
2025-01-20 09:34:47 -0800

    Create blog-4.vue

---
+++ pages/blog-4.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1> Day 4 </h1>
+  <p> I just realized that i've been such a selfish </p>
+  <p> I just wanted to buy everything that i want </p>
+  <p> Consume everything that i want </p>
+  <p> Know i got it </p>
+  <i> When you have more, you got less </i>
+  <i> This is my confession, this is my pain </i> 
+</template>
