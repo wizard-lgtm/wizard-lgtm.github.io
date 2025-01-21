99154f34bcc40c4a68f62f45ddec117919920cb1
J <rippiten23@gmail.com>
2025-01-21 14:45:35 -0800

    Create blog-6.vue

---
+++ pages/blog-6.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1> Day 6 </h1>
+  <p> Started to work again. Let's see what is going to be on </p>
+  <p> Started going to gym again. Maybe that will fix something. <p>
+  <p> Idk, i'm trying. To be more </p>
+  <a href="https://riscv.org/specifications/ratified/" /> 
+</template> 
