84c5a6ae8ba02f8df5ec58acb45112ea507ee32c
J <rippiten23@gmail.com>
2025-01-20 17:18:06 -0800

    Create blog-5.vue

---
+++ pages/blog-5.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1> Day 5 </h1>
+  <p> I think that old tech was better, i mean back then i had some kinda normal 60hz 1920 to 1080 60hz oldschool monitor and i was happy with it. </p>
+  <p> I had no issue with my old computer, a desktop, now i bought i laptop which it was good because now i can code also in school. So it was a good choice to buy it </p>
+  <p> But after that i wanted a macbook so much, i don't really know why but it was kinda an a obsession to me. I really wanted it and i talked with my parents for 4 months. Everybody said don't buy it. I still bought that costs a lot.  </p>
+  <p> My mom said it's okay but it was obvious we can't affort it, i'm still thinking that mom is having hard time to pay that bills </p>
+  <p> You wanna hear about the worst thing? I'm still using my old laptop and even my old desktop over macbook, because it's doing. I'm hating myself because of it </p>
+  <p> I guess i'm a horrible person. I am a terrible person. I am a jerk </p>
+  <p> I'm sorry mom. </p>
+</template>
+
+<style scoped>
+  body{
+    background-color: #111111;
+    color: #fafafa;
+  }
+</style>
