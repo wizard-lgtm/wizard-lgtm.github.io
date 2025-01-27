9c3611003fdb1c84e5ab5fe4336cec90f0f12bbb
J <rippiten23@gmail.com>
2025-01-26 18:06:11 -0800

    Create blog-7.vue

---
+++ pages/blog-7.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1> Day 7 - I've beaten the monster </h1>
+  <p> That is true, i have been finally beaten the monster </p>
+  <p> I finally quit playing that game </p>
+  <p> Now the future is bright, i can see it </p>
+</template>
